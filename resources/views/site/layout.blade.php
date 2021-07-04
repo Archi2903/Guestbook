bfb5d49c518c05709948c9da9f51afe82a98811f
Mona Lisa <77768569+Archi2903@users.noreply.github.com>
2021-07-04 17:16:55 +0300

    #1 create new branch JavaScript

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -55,12 +55,12 @@
 
 <main>
     <div class="row ">
-        <div class="col-sm-1">
+        <div class="col-sm-2">
         </div>
-        <div class="col-sm-10">
+        <div class="col-sm-8">
             @yield('main_content')
         </div>
-        <div class="col-sm-1">
+        <div class="col-sm-2">
         </div>
     </div>
 </main>
